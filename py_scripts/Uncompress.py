843929084e3854654541e111fa11eafd0edececc
kevin <kevin@kevin-G46VW.(none)>
2013-07-07 10:59:00 -0500

    word freq

--- py_scripts/Uncompress.py
+++ py_scripts/Uncompress.py
@@ -25,20 +25,36 @@ english_stop_words = english_stop_words.union(py_keywords, c_keywords, cpp_keywo
 
 
 def parseFolder(srcFolder):
-    for (path, subfolder, files) in os.walk(srcFolder):
-        for file_name in files:
+    for project in os.listdir(srcFolder):
+        path = srcFolder + "//" + project
+        for file_name in os.listdir(path):
             if re.search(r"(zip|tar.gz|jar)", file_name):
                 print path + "//" + file_name
-                
+
+                Archive(path + "//" + file_name).extractall(path)
+                doc_keyword_dict, total_count_doc = calculateDocumentFrequency(path)
+                code_keyword_dict, total_count_code = calculateSrcFrq(path)
+
+                if doc_keyword_dict and code_keyword_dict:
+                    common_word_set = set(code_keyword_dict.keys()).intersection(set(doc_keyword_dict.keys()))
+                    result = open(path + "//" + "result.txt", "w")
+                    
+                    for word in common_word_set:
+                        result.write(" word = %s | frequency in documentation = %d | frequency in source code = %d | \n" %(word,
+                        doc_keyword_dict[word], code_keyword_dict[word]))
+
+                    result.close()
+
 
 def calculateDocumentFrequency(srcFolder):
     doc_keyword_count = {}
     doc_keyword_count = defaultdict(lambda: 0)
+    total_count = 0
 
     for (path, subfolder, files) in os.walk(srcFolder):
         for file_name in files:
             if re.search(r"(.+\.txt|.+\.readme|.+\.html)", file_name):
-                print file_name
+                print "reading documnet  " + file_name
                 src_file_path = path + "//" + file_name
 
                 with open(src_file_path) as document:
@@ -46,9 +62,11 @@ def calculateDocumentFrequency(srcFolder):
                         line = line.strip().translate(trantab)  # remove punctuations
                         for unique_word in filter(lambda x: x not in english_stop_words, map(lambda x: x.lower(), line.split())):
                             doc_keyword_count[unique_word] += 1
+                            total_count += 1
 
     # for word in sorted(doc_keyword_count, key=doc_keyword_count.get, reverse=True):
     #     print word, doc_keyword_count[word]
+    return doc_keyword_count, total_count
 
 
 def calculateSrcFrq(srcFolder):
@@ -58,28 +76,27 @@ def calculateSrcFrq(srcFolder):
     * remove certain punctuations
     for now it handles .py .c .cpp and .java files
     '''
-    
     src_keyword_count = {}
     src_keyword_count = defaultdict(lambda: 0)
+    total_count = 0
 
     for (path, subfolder, files) in os.walk(srcFolder):
-        
-        for file_name in files:
+        for file_name in files:          
             
-            
-            if re.search(r"(.+\.py|.+\.cpp|.+\.c|.+\.java)", file_name):
-                print file_name
+            if re.search(r"(.+\.py$|.+\.cpp$|.+\.c$|.+\.java$|.+\.h$)", file_name):
+                
                 stop_words = set([])
                 
-                if re.search(r".py", file_name):
+                if re.search(r".+\.py$", file_name):
                     stop_words = py_keywords
-                elif re.search(r".cpp", file_name):
+                elif re.search(r".+\.cpp$", file_name):
                     stop_words = cpp_keywords
-                elif re.search(r".c", file_name):
+                elif re.search(r".+\.c$", file_name) or re.search(r".+\.h$", file_name):
                     stop_words = c_keywords
                 else:
                     stop_words = java_keywords
                 
+                print "reading src code  " + file_name
                 src_file_path = path + "//" + file_name
 
                 with open(src_file_path) as src_code:
@@ -87,9 +104,11 @@ def calculateSrcFrq(srcFolder):
                         line = line.strip().translate(trantab)  # remove punctuations
                         for unique_word in filter(lambda x: x not in stop_words, map(lambda x: x.lower(), line.split())):
                             src_keyword_count[unique_word] += 1
+                            total_count += 1
 
-    for word in sorted(src_keyword_count, key=src_keyword_count.get, reverse=True):
-        print word, src_keyword_count[word]
+    # for word in sorted(src_keyword_count, key=src_keyword_count.get, reverse=True):
+    #     print word, src_keyword_count[word]
+    return src_keyword_count,total_count
 
 
 if __name__ == '__main__':
