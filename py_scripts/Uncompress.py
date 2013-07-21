1e78ae2ca9e450ea9ae5edaaaaa66dbbe2c28e44
kevin <kevin@kevin-G46VW.(none)>
2013-07-21 17:22:05 -0500

    adding ngrams

--- py_scripts/Uncompress.py
+++ py_scripts/Uncompress.py
@@ -10,9 +10,10 @@ import shutil
 import re
 from collections import defaultdict
 from string import maketrans
+import math
 
 base_folder = "//home//kevin//workspace//SourceForge//final"
-punctuations = "[!\"#$%&'()*+,./:;<=>?@[\]^`{|}]'"
+punctuations = "[!\"#$%&'()*+,./:;<=>?@[\]^`{|}]'-"
 trantab = maketrans(punctuations, " " * len(punctuations))
 
 py_keywords = set(word.strip() for word in open("python.txt"))
@@ -27,54 +28,85 @@ english_stop_words = english_stop_words.union(py_keywords, c_keywords, cpp_keywo
 def parseFolder(srcFolder):
     for project in os.listdir(srcFolder):
         path = srcFolder + "//" + project
+        
         for file_name in os.listdir(path):
-            if re.search(r"(zip|tar.gz|jar)", file_name):
-                print path + "//" + file_name
-
-                Archive(path + "//" + file_name).extractall(path)
-                doc_keyword_dict, total_count_doc = calculateDocumentFrequency(path)
-                code_keyword_dict, total_count_code = calculateSrcFrq(path)
-
-                if doc_keyword_dict and code_keyword_dict:
-                    common_word_set = set(code_keyword_dict.keys()).intersection(set(doc_keyword_dict.keys()))
-                    result = open(path + "//" + "result.txt", "w")
+            if re.search(r"(zip$|tar.gz$|jar$)", file_name):
+                try:
+                    Archive(path + "//" + file_name).extractall(path)
                     
-                    for word in common_word_set:
-                        result.write(" word = %s | frequency in documentation = %d | frequency in source code = %d | \n" %(word,
-                        doc_keyword_dict[word], code_keyword_dict[word]))
+                    doc_keyword_dict, total_word_count_doc, total_document_count = calculateDocumentFrequency(path)
+                    code_keyword_dict, total_count_code = calculateSrcCodeFrq(path)
 
-                    result.close()
+                    if doc_keyword_dict and code_keyword_dict:
+                        common_word_set = set(code_keyword_dict.keys()).intersection(set(doc_keyword_dict.keys()))
+                        result_file = open(srcFolder + "//" + "result"+"//" + project + ".txt", "w")
+                        result_dict = {}
+                        
+                        result_file.write("Project folder name = %s \n" %(project))
+                        result_file.write("********************************************************************************************** \n\n")
+
+                        for word in common_word_set:
+                            result_dict[word] = code_keyword_dict[word]
+                            # result_dict[word] = (doc_keyword_dict[word][0] / float(total_word_count_doc) ) * math.log10(total_document_count/ float(doc_keyword_dict[word][1])) 
+                       
+                         
+                        for word in sorted(result_dict,key=result_dict.get,reverse=True):
+                            tfidf = (doc_keyword_dict[word][0] / float(total_word_count_doc) ) * math.log10(total_document_count/ float(doc_keyword_dict[word][1])) 
+                            result_file.write(" word = %s | frequency in documentation = %d | frequency in source code = %d | tf-idf = %f \n" %(word, doc_keyword_dict[word][0], code_keyword_dict[word], tfidf))
+
+                        result_file.close()
+                except:
+                    print "***********error for folder*********** " +project
 
 
 def calculateDocumentFrequency(srcFolder):
+    """
+    @return
+    dict = key = word , [freq of the word , number of documents it was found]
+    integer = total number of words 
+    integer = total number of documents
+    """
     doc_keyword_count = {}
-    doc_keyword_count = defaultdict(lambda: 0)
-    total_count = 0
+    doc_keyword_count = defaultdict(lambda: [0,0])
+    total_word_count = 0
+    total_document_count = 0
 
     for (path, subfolder, files) in os.walk(srcFolder):
         for file_name in files:
-            if re.search(r"(.+\.txt|.+\.readme|.+\.html)", file_name):
-                print "reading documnet  " + file_name
+            if re.search(r"(.+\.txt$|.+\.readme$|.+\.html$)", file_name):
+
                 src_file_path = path + "//" + file_name
 
+                words_in_file = set([])
                 with open(src_file_path) as document:
                     for line in document:
                         line = line.strip().translate(trantab)  # remove punctuations
                         for unique_word in filter(lambda x: x not in english_stop_words, map(lambda x: x.lower(), line.split())):
-                            doc_keyword_count[unique_word] += 1
-                            total_count += 1
+                            doc_keyword_count[unique_word][0] += 1
+                            words_in_file.add(unique_word)
+                            total_word_count += 1
+
+                for word in words_in_file:
+                    doc_keyword_count[word][1] += 1
+
+                total_document_count += 1
+
 
     # for word in sorted(doc_keyword_count, key=doc_keyword_count.get, reverse=True):
     #     print word, doc_keyword_count[word]
-    return doc_keyword_count, total_count
+    return doc_keyword_count, total_word_count, total_document_count
 
 
-def calculateSrcFrq(srcFolder):
+def calculateSrcCodeFrq(srcFolder):
     ''' pass the src folder and it creates a frequency count of the words
     occuring in the source file
     * remove the keywords
     * remove certain punctuations
     for now it handles .py .c .cpp and .java files
+
+    @return
+    dict = key = word , freq in src
+    integer = total words in src
     '''
     src_keyword_count = {}
     src_keyword_count = defaultdict(lambda: 0)
@@ -96,7 +128,7 @@ def calculateSrcFrq(srcFolder):
                 else:
                     stop_words = java_keywords
                 
-                print "reading src code  " + file_name
+                # print "reading src code  " + file_name
                 src_file_path = path + "//" + file_name
 
                 with open(src_file_path) as src_code:
@@ -108,8 +140,9 @@ def calculateSrcFrq(srcFolder):
 
     # for word in sorted(src_keyword_count, key=src_keyword_count.get, reverse=True):
     #     print word, src_keyword_count[word]
-    return src_keyword_count,total_count
+    return src_keyword_count, total_count
 
 
 if __name__ == '__main__':
-    parseFolder("//home//kevin//workspace//SourceForge//py_scripts//test")
+    parseFolder("//home//kevin//workspace//SourceForge//final")
+    # parseFolder("//home//kevin//workspace//SourceForge//py_scripts//test")
